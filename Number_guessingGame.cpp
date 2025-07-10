450e4f18b9150cb1e9c0137b74ad2cd20cc8928f
Shivani Kumari <shivaniprasad0216@gmail.com>
2025-07-10 17:08:19 +0530

    Add files via upload

---
+++ Number_guessingGame.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main();
+
+// function for asking again and again
+void asking()
+{
+    cout << "do you want to play again for Yes (1) and for No (0)\n";
+    int ask;
+    cin >> ask;
+    if (ask == 0)
+    {
+        cout << "Game End\n";
+    }
+    else
+    {
+        main();
+    }
+}
+// main function of the whole program.
+int guess = 0;
+int main()
+{
+    int user_input;
+    int random_no = rand() % 100 + 1;
+    cout << "enter any no in range between 1 to 100:\n";
+    while (guess != random_no)
+    {
+        cin >> user_input;
+        if (random_no < user_input)
+        {
+            cout << "Too high,Try again\n";
+        }
+        else if (random_no > user_input)
+        {
+            cout << "To low,Try again\n";
+        }
+        else
+        {
+            if (user_input == random_no)
+            {
+                cout << "Congratulation!, You guessed the number:\n";
+                asking();
+                break;
+            }
+        }
+    }
+    return 0;
+}
